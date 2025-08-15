50e1670945d5caf1866854e4f422dd852744371b
Mohamed Triki Jouhrati <mohamedtrikijouhrati@gmail.com>
2025-08-15 02:27:42 +0100

    Changed and fixed basic functionality for adding and deleting task (improvements required)

--- main.py
+++ main.py
@@ -25,43 +25,59 @@ def write_json(new_data, filename='data.json'):
 def option_1(filename='data.json'):
     # Current date
     date = datetime.datetime.now()
-
-    print("ADD A TASK")
-
-    # Getting User Input
-    taskDescription = input("What is the task about:" + "\n")
-
+    
     # Loading data
     with open(filename, 'r+') as file:
         file_data = json.load(file)
 
+    print("ADD A TASK")
+
+    # Getting User Input
+    loop = True
+    ids = []
+    while loop:
+        taskId = int(input("What ID do you want to give to this task:" + "\n"))
+        for i in file_data["tasks"]:
+            ids.append(i.get("id"))
+        if taskId in ids:
+            print("That already exists")
+        else:
+            loop = False
+                
+                
+    taskDescription = input("What is the task about:" + "\n")
+
     # Task Structure
     task = {
-        "id": len(file_data["tasks"]) + 1,
+        "id": taskId,
         "description": taskDescription,
         "status": "todo",
         "createdAt": str(date.strftime("%d%b%Y")),
         "updatedAt": "dateUpdatedAt"
     }
-
+    
     # Calling function to append data
     write_json(task)
+    
 
 
 # Delete task from JSON file (Option 2)
 def option_2(filename='data.json'):
     print("DELETE A TASK")
-
+    
     # Getting User Input and using it as id of the task
-    taskId = int(
-        input("Type the ID of the task you would like to delete" + "\n"))
+    taskId = int(input("Type the ID of the task you would like to delete" + "\n"))
 
     # Loading data
     with open(filename, 'r+') as file:
         file_data = json.load(file)
 
         # Removing 'task' from list
-        file_data["tasks"].pop(taskId - 1)
+        for i in file_data["tasks"]:
+            if taskId == i.get("id"):
+                file_data["tasks"].remove(i)
+
+            
 
         # Write the updated data back to the file
         with open(filename, 'w') as file:
@@ -71,7 +87,7 @@ def option_2(filename='data.json'):
 
 
 def option_3():
-    print("You selected Option 3.")
+    print("UPDATE TASK")
 
 
 def option_4():
@@ -81,8 +97,8 @@ def option_4():
 def main_menu():
     print("What do you want to do!")
     print("1. Add a task")
-    print("2. Update a task")
-    print("3. Delete a task")
+    print("2. Delete a task")
+    print("3. Update a task")
     print("4. Option 4")
     print("0. Exit")
 
