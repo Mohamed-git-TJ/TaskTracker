cc0adef7d56488661e92e0d8816f913c22937c8e
Mohamed Triki Jouhrati <mohamedtrikijouhrati@gmail.com>
2025-08-29 00:42:43 +0100

    Improved readability and organization for better understanding

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import datetime
 
 # TASK TRACKER CLI
 
-
 # Append new data to JSON file
 def write_json(new_data, filename='data.json'):
+    
     with open(filename, 'r+') as file:
         # Load data
         file_data = json.load(file)
@@ -43,8 +43,7 @@ def option_1(filename='data.json'):
             print("That already exists")
         else:
             loop = False
-                
-                
+                        
     taskDescription = input("What is the task about:" + "\n")
     status = "todo"
     
@@ -60,8 +59,7 @@ def option_1(filename='data.json'):
     # Calling function to append data
     write_json(task)
     
-
-
+    
 # Delete task from JSON file (Option 2)
 def option_2(filename='data.json'):
     print("DELETE A TASK")
@@ -86,20 +84,24 @@ def option_2(filename='data.json'):
 
     print("Task deleted succesfully")
 
-
+# Update a task of JSON file (Option 3)
 def option_3(filename='data.json'):
     print("UPDATE TASK")
     date = datetime.datetime.now()
+    
     # Loading data
     with open(filename, 'r+') as file:
         file_data = json.load(file)
-        
+       
+    # Getting User Input and using it as id of the task 
     taskId = input("Type the ID of the task you would like to edit/update" + "\n")
 
     # Removing 'task' from list
     for i in file_data["tasks"]:
+        
         if taskId == i.get("id"):
             file_data["tasks"].remove(i)
+            
             # Write the updated data back to the file
             with open(filename, 'w') as file:
                 json.dump(file_data, file, indent=4)
@@ -113,24 +115,24 @@ def option_3(filename='data.json'):
         "createdAt": i.get("createdAt"),
         "updatedAt": str(date.strftime("%d/%b/%Y %H:%M"))
     }
+    
     # Calling function to append data
     write_json(task)
             
-            
-            
-    
-            
-           
-            
-
+                
+# Listing all tasks from JSON file (Option 4)
 def option_4(filename='data.json'):
     
+    # Loading data
     with open(filename, 'r+') as file:
         file_data = json.load(file)
     
+    # Setting structure of the headers of the list
     header = ["ID", "DESCRIPTION", "STATUS", "DATE OF CREATION", "DATE OF UPDATE"]
 
+    # Defining the size of the cells of the list
     def fixed_length(text, length):
+        
         if len(text) > length:
             text = text[:length]
         elif len(text) < length:
@@ -138,14 +140,18 @@ def option_4(filename='data.json'):
         return text
     
     
-    
+    # Function to show all tasks
     def print_allTasks():
+        
+        # List design
         print("-"*114)
         for colum in header:
+            
             print(fixed_length(colum, 20), end = " | ")
         print()
         print("-"*114)
         
+        # List with all task values to be show in each row
         task = []
         for row in file_data["tasks"]:
                     
@@ -155,7 +161,7 @@ def option_4(filename='data.json'):
             task.append(row.get("createdAt"))
             task.append(row.get("updatedAt"))
             
-        
+            # Column desing with task values
             for colum in task:
                 
                 print(fixed_length(colum, 20), end = " | ")
@@ -163,13 +169,17 @@ def option_4(filename='data.json'):
             print("-"*114)
             task.clear()
             
+    # Function to show tasks that are done
     def print_doneTasks():
+        
+        # List design
         print("-"*114)
         for colum in header:
             print(fixed_length(colum, 20), end = " | ")
         print()
         print("-"*114)
         
+        # List with all task values to be show in each row
         task = []
         for row in file_data["tasks"]:
                     
@@ -179,21 +189,28 @@ def option_4(filename='data.json'):
             task.append(row.get("createdAt"))
             task.append(row.get("updatedAt"))
             
-        
+            # Column desing with task values
             for colum in task:
+                
                 if row.get("status") == "done":
+                    
                     print(fixed_length(colum, 20), end = " | ")
             print()
             print("-"*114)
             task.clear()
     
+    # Function to show tasks that are in progress
     def print_inProgressTasks():
+        
+        # List design
         print("-"*114)
         for colum in header:
+            
             print(fixed_length(colum, 20), end = " | ")
         print()
         print("-"*114)
         
+        # List with all task values to be show in each row
         task = []
         for row in file_data["tasks"]:
                     
@@ -203,21 +220,29 @@ def option_4(filename='data.json'):
             task.append(row.get("createdAt"))
             task.append(row.get("updatedAt"))
             
-        
+            # Column desing with task values
             for colum in task:
+                
                 if row.get("status") == "in-progress":
+                    
                     print(fixed_length(colum, 20), end = " | ")
             print()
             print("-"*114)
             task.clear()
-    
+            
+    # Function to show tasks that are to be done
     def print_todoTasks():
+        
+        # List design
         print("-"*114)
         for colum in header:
+            
             print(fixed_length(colum, 20), end = " | ")
+            
         print()
         print("-"*114)
         
+        # List with all task values to be show in each row
         task = []
         for row in file_data["tasks"]:
                     
@@ -227,14 +252,17 @@ def option_4(filename='data.json'):
             task.append(row.get("createdAt"))
             task.append(row.get("updatedAt"))
             
-        
+            # Column desing with task values
             for colum in task:
+                
                 if row.get("status") == "todo":
+                    
                     print(fixed_length(colum, 20), end = " | ")
             print()
             print("-"*114)
             task.clear()
-            
+          
+    # Menu to choose the type of list to be prompted to the user
     while True:
         choice = input("What list would you like to see 1, 2, 3 or 4: ")
         
@@ -252,21 +280,27 @@ def option_4(filename='data.json'):
             print("Invalid choice. Please try again.")
         
         
+# Set status of tasks in JSON file (Option 5)
 def option_5(filename='data.json'):
+    
+    # Loading data
     with open(filename, 'r+') as file:
         file_data = json.load(file)
         
-    
+    # Getting User Input and using it as id of the task 
     taskId = input("Type the ID of the task you would like to mark down" + "\n")
     
     # Removing 'task' from list
     for i in file_data["tasks"]:
+        
         if taskId == i.get("id"):
             file_data["tasks"].remove(i)
+            
             # Write the updated data back to the file
             with open(filename, 'w') as file:
                 json.dump(file_data, file, indent=4)
-            
+                
+    # Setting status value
     status = ""
     while True:
         statusChoice = input("Would you like to mark the task as 'in-progress' or 'done' \n Type 1 or 2: ")
@@ -288,17 +322,15 @@ def option_5(filename='data.json'):
         "createdAt": i.get("createdAt"),
         "updatedAt": i.get("updatedAt")
     } 
+    
     # Calling function to append data
     write_json(task)
      
-    
-
-
-
+# Main menu
 def main_menu():
+    
     print("What do you want to do!")
     
-
     while True:
         print("1. Add a task")
         print("2. Delete a task")
@@ -306,8 +338,9 @@ def main_menu():
         print("4. Tasks List")
         print("5. Tasks status")
         print("0. Exit")
-        choice = input("Enter your choice (0-5): ")
-
+    
+        # User input to choose option
+        choice = input("Enter your choice (0-5): ")    
         if choice == "1":
             option_1()
         elif choice == "2":
@@ -324,7 +357,7 @@ def main_menu():
         else:
             print("Invalid choice. Please try again.")
 
-
+# Checking if file exits and creating one if it does not
 if os.path.isfile('data.json'):
     main_menu()
 else:
